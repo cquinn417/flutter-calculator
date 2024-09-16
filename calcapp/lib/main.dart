b4498531e8cdea586a4615af43398130638388d2
Colleen Quinn <cquinn417@outlook.com>
2024-09-16 16:32:25 -0400

    Initial commit of the Flutter calculator app

---
+++ calcapp/lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Colleen Quinn\'s Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _expression = '';
+  String _result = '';
+
+  void _onPressed(String text) {
+    setState(() {
+      if (text == 'C') {
+        _expression = '';
+        _result = '';
+      } else if (text == '⌫') {
+        if (_expression.isNotEmpty) {
+          _expression = _expression.substring(0, _expression.length - 1);
+        }
+      } else if (text == '=') {
+        try {
+          final expression = Expression.parse(_expression);
+          final evaluator = const ExpressionEvaluator();
+          final result = evaluator.eval(expression, {});
+          _result = result.toString();
+        } catch (e) {
+          _result = 'Error';
+        }
+      } else {
+        _expression += text;
+      }
+    });
+  }
+
+  Widget _buildButton(String text, {Color color = Colors.black}) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(4),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(20), backgroundColor: color,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          onPressed: () => _onPressed(text),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Colleen Quinn\'s Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              alignment: Alignment.bottomRight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    _expression,
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text(
+                    _result,
+                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                children: <Widget>[
+                  _buildButton('7'),
+                  _buildButton('8'),
+                  _buildButton('9'),
+                  _buildButton('/', color: Colors.orange),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton('4'),
+                  _buildButton('5'),
+                  _buildButton('6'),
+                  _buildButton('*', color: Colors.orange),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton('1'),
+                  _buildButton('2'),
+                  _buildButton('3'),
+                  _buildButton('-', color: Colors.orange),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton('0'),
+                  _buildButton('C', color: Colors.red),
+                  _buildButton('⌫', color: Colors.blue),
+                  _buildButton('=', color: Colors.green),
+                  _buildButton('+', color: Colors.orange),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
